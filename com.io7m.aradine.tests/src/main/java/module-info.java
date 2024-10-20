4285f8f63819be7a37357e07598053326e70e29a
Mark Raynsford <code@io7m.com>
2024-10-20 10:58:55 +0000

    Use junit BOM.

--- com.io7m.aradine.tests/src/main/java/module-info.java
+++ com.io7m.aradine.tests/src/main/java/module-info.java
@@ -59,8 +59,9 @@ open module com.io7m.aradine.tests
   requires org.mockito;
   requires org.slf4j;
 
-  requires transitive org.junit.jupiter.api;
-  requires transitive org.junit.jupiter.engine;
-  requires transitive org.junit.platform.commons;
-  requires transitive org.junit.platform.engine;
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+  requires org.junit.platform.commons;
+  requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 }
